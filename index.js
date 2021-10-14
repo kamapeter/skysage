a1b12555665874cb6d30c0e427cafdd4a5ce61a4
kamapeter <porji54@yahoo.com>
2021-10-14 21:39:09 +0100

    pos

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const params = {
   const secret = "90b0ed4b46299c3d64868f62aaf5933ba2ffd3b3db1637c4b20be02018b748feab99f0b87c6b5890891f5bcf56b4a57fbf08be13c72838d887c53681ce5945833eeffc7dae81d5b29e0803a188877960d61e91021ae2409c848e3021d3822f1a28a632b3a0b5c08134868bef11952a77";
   
 
-  axios.get('https://api.astronomyapi.com/api/v2/bodies',{
+  axios.get('https://api.astronomyapi.com/api/v2/bodies/positions',{
   params: {
     longitude: "139.696233",
     latitude: "35.570431",
