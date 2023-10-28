353542967d4d76e71802368067cb8cb57931cbe4
p <p>
2023-10-29 00:12:26 +0100

    update approvePay.js

--- netlify/functions/approvePay/approvePay.js
+++ netlify/functions/approvePay/approvePay.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 export const handler = async (event, context) => {
   const payId = JSON.parse(event.body)
-  const platformAPI = `api.minepi.com/v2/payments/${payId.payId}/approve`
+  const platformAPI = `https://api.minepi.com/v2/payments/${payId.payId}/approve`
 
   var res = await fetch(
     platformAPI,{
