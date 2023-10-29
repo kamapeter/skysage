4743a46053ec6fa80632bdfbd5f812acff63b4f1
p <p>
2023-10-29 01:21:06 +0100

    update new.js

--- netlify/functions/new/new.js
+++ netlify/functions/new/new.js
@@ -5,7 +5,7 @@ export const handler = async (event, context) => {
   const eventBody = JSON.parse(event.body)
   const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.region
 
-  const response = await axios.post(POKE_API)
+  const response = await axios.get(POKE_API)
   const data = response
   return {
     statusCode: 200,
