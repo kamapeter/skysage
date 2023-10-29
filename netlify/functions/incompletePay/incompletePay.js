d6452a4a04efe4edd58f4d147d83f19933490c92
p <p>
2023-10-29 03:38:56 +0100

    update incompletePay.js

--- netlify/functions/incompletePay/incompletePay.js
+++ netlify/functions/incompletePay/incompletePay.js
@@ -2,9 +2,10 @@ import fetch from 'node-fetch'
 const axios = require('axios');
 export const handler = async (event, context) => {
   const payment = JSON.parse(event.body);
-  const payId = payment.identifier;
+  //const payId = payment.identifier;
   //const txid = payment.transaction.txid;
-  const txid = "7cb39f257c3a675b9ab43 d4f7f2284907f708e2bb804cb4c 8dd77f4bdaaa7fa6";
+  const payId = "uaHVqgqolhISKNV1WLGnGSRCPOCw"
+  const txid = "7cb39f257c3a675b9ab43d4f7f2284907f708e2bb804cb4c8dd77f4bdaaa7fa6";
   console.log(payId,txid)
   const platformAPI = `https://api.minepi.com/v2/payments/${payId}/complete`
 
